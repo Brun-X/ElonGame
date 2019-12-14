6412d8711dabbdbde35fcca76502ac71614001c6
Your Name <you@example.com>
2019-12-14 09:07:41 +0100

    i've come a looong way this morning

--- balls/balls.cpp
+++ balls/balls.cpp
@@ -21,214 +21,18 @@ IMPLEMNTED WITH THE OLCPIXELGAMEENGINE FROM:
 typedef std::pair <float, float> fPair;
 typedef std::pair <int, int> iPair;
 
+int circlePoints = 45;
 
-struct Ball
+class Object
 {
-	float px;
-	float py;
-	float vx;
-	float vy;
-	float radius;
-};
-
-class Pico : public olc::PixelGameEngine
-{
-private:
-	int levelWidth;
-	int levelHeight;
-	std::string levelTiles;
-	int tileWidth = 8;
-	int tileHeight = 8;
-
-	std::vector<fPair> modelCircle;
-	std::vector<Ball*> balls;
-	int circlePoints = 45;
-	float scaleFactor = 10.0;
-
-	std::vector< std::vector<iPair> > lines;
-
 public:
-	Pico()
-	{
-		sAppName = "Pico";
-	}
+	std::vector<std::vector<iPair>> objectLines_;
+	
+	virtual ~Object() {}
 
-	bool OnUserCreate() override
-	{
+	virtual void calculateShape() {}
 
-		modelCircle.push_back({ 0.0f, 0.0f });
-		
-		for(int i = 0; i <= circlePoints; i++)
-		{
-			modelCircle.push_back({ cosf(i / (float)(circlePoints -1) * 2.00f * 3.14159f) , sinf(i / (float)(circlePoints - 1) * 2.0f * 3.14159f) });
-		}
-
-		Ball* b1 = new Ball{100.0, 100.0, 0.0, 0.0, 50.0};
-		balls.push_back(b1);
-
-		
-
-		levelWidth = 32;
-		levelHeight = 32;
-		levelTiles += "################################";
-		levelTiles += "#..............................#";
-		levelTiles += "#..............................#";
-		levelTiles += "#..............................#";
-		levelTiles += "#..............................#";
-		levelTiles += "#..............................#";
-		levelTiles += "#..............................#";
-		levelTiles += "#..............................#";
-		levelTiles += "#..............................#";
-		levelTiles += "#..............................#";
-		levelTiles += "#..............................#";
-		levelTiles += "#..............................#";
-		levelTiles += "#..............................#";
-		levelTiles += "#..............................#";
-		levelTiles += "#..............................#";
-		levelTiles += "#..............................#";
-		levelTiles += "#..............................#";
-		levelTiles += "#..............................#";
-		levelTiles += "#..............................#";
-		levelTiles += "#..............................#";
-		levelTiles += "#..............................#";
-		levelTiles += "#..............................#";
-		levelTiles += "#..............................#";
-		levelTiles += "#..............................#";
-		levelTiles += "#..............................#";
-		levelTiles += "#..............................#";
-		levelTiles += "#..............................#";
-		levelTiles += "#..............................#";
-		levelTiles += "#..............................#";
-		levelTiles += "#..............................#";
-		levelTiles += "#..............................#";
-		levelTiles += "################################";
-
-		for(int x = 0; x < ScreenWidth(); x++)
-		{
-			for(int y = 0; y < ScreenHeight(); y++)
-			{
-				Draw(x, y, olc::BLUE);
-			}
-		}
-
-
-		for(auto ball : balls)
-		{
-			for(int s = 0; s <= circlePoints; s++)
-			{
-				float lastX, lastY;
-				
-				float pointX = (modelCircle[s].first * ball->radius) + ball->px;
-				float pointY = (modelCircle[s].second * ball->radius) + ball->py;
-
-				if(s > 1) makeLine(lastX, lastY, pointX, pointY, olc::BLACK);
-
-				else makeLine(ball->px, ball->py, pointX, pointY, olc::BLACK);
-		
-				lastX = pointX;
-				lastY = pointY;
-
-				Draw(pointX, pointY, olc::WHITE);
-			}
-		}
-
-		//makeLine(0.0f, 200.0f, 200.0f, 200.0f, olc::WHITE);
-
-		SetPixelMode(olc::Pixel::NORMAL);
-
-		return true;
-	}
-
-	bool OnUserUpdate(float fElapsedTime) override
-	{
-		//User input
-/*
-		if(GetKey(olc::Key::UP).bHeld) balls[0]->py -= 2;
-		if(GetKey(olc::Key::DOWN).bHeld) balls[0]->py += 2;
-		if(GetKey(olc::Key::RIGHT).bHeld) balls[0]->px += 2;
-		if(GetKey(olc::Key::LEFT).bHeld) balls[0]->px -= 2;		
-*/		
-		//Collision detection
-
-
-		//Snap Sprite
-
-		/*
-		float tileX = GetMouseX() / tileWidth;
-		float tileY = GetMouseY() / tileHeight;
-
-		int snapX = (tileX - (int)tileX) < 0.5 ? (int)tileX : (int)tileX + 1; 
-		int snapY = (tileY - (int)tileY) < 0.5 ? (int)tileY : (int)tileY + 1; 
-
-		elonPos.x = (snapX * tileWidth) - (elonPos.w / 2);
-		elonPos.y = (snapY * tileHeight) - (elonPos.h / 2);
-		*/
-		/*
-		auto adjustAngle = [](Ball* b, float rotation)
-		{
-			b->vx = (cosf(rotation) * 2.0f * 3.13159f) * b->radius;
-			b->vy = (sinf(rotation) * 2.0f * 3.13159f) * b->radius;
-			//return b;
-		} */
-
-		for(auto ball : balls)
-		{
-			for(int s = 0; s <= circlePoints; s++)
-			{
-				float lastX, lastY;
-
-				float pointX = (modelCircle[s].first * ball->radius) + ball->px;
-				float pointY = (modelCircle[s].second * ball->radius) + ball->py;
-
-				if(s > 1) makeLine(lastX, lastY, pointX, pointY, olc::BLACK);
-
-				else
-				{
-					float rotation = 225.0f;
-					ball->vx = cosf(rotation) * ball->radius + ball->px;
-					ball->vy = -sinf(rotation) * ball->radius + ball->py;
-					
-					//adjustAngle(temp, 0.5); 
-					makeLine(ball->px, ball->py, ball->vx, ball->vy, olc::BLACK);
-				} 
-		
-				lastX = pointX;
-				lastY = pointY;
-
-				Draw(pointX, pointY, olc::WHITE);
-			}
-		}
-
-
-
-		//Draw Screen
-
-/*
-		
-		for(int x = 0; x < levelWidth; x++)
-		{
-			for(int y = 0; y < levelHeight; y++)
-			{
-				olc::Pixel p = levelTiles[y * levelWidth + x] == '#' ? olc::DARK_RED : olc::BLUE;
-				FillRect(x * tileWidth, y * tileHeight, tileWidth, tileHeight, p);
-			}
-		}
-*/
-
-		
-
-		for(auto vec : lines)
-		{
-			for(auto l : vec)
-			{
-				Draw(l.first, l.second, olc::BLACK);
-			}
-		}
-
-		return true;
-	}
-
-	bool makeLine(float x1, float y1, float x2, float y2, olc::Pixel p)
+	bool makeLine(float x1, float y1, float x2, float y2, std::vector<std::vector<iPair>>* v)
 	{
 		std::vector<iPair> line;
 		float gradientX, gradientY;
@@ -241,8 +45,6 @@ public:
 
 		gradientX = dx != 0.0f ? dx / dy : 0.0f;
 		gradientY = dy != 0.0f ? dy / dx : 0.0f;
-
-
 
 		float tmp = 0.000;
 		float round = 0.8;
@@ -334,10 +136,6 @@ public:
 				}
 			}
 		}
-
-
-
-
 	
 		if(dx == 0.0f)
 		{
@@ -357,9 +155,215 @@ public:
 			}
 		}
 
-		lines.push_back(line);
+		v->push_back(line);
 		return true;
 	}
+
+};
+
+class Ball : public Object
+{
+public:
+	Ball() : Object() {}
+
+	Ball(float px, float py, float vx, float vy, float radius, float rotation) : px_(px), py_(py), vx_(vx), vy_(vy), radius_(radius), rotation_(rotation)
+	{
+		mc_.push_back({ 0.0f, 0.0f });
+		
+		for(int i = 0; i <= circlePoints; i++)
+		{
+			mc_.push_back({ cosf(i / (float)(circlePoints -1) * 2.00f * 3.14159f) , sinf(i / (float)(circlePoints - 1) * 2.0f * 3.14159f) });
+		}
+	}
+
+	virtual void calculateShape() override 
+	{
+		float pointX, pointY;
+		float lastX = mc_[1].first * radius_ + px_;
+		float lastY = mc_[1].second * radius_ + py_;
+		objectLines_.clear();
+
+		for(int s = 0; s < circlePoints + 1; s++)
+		{
+			if(s > 1) 
+			{
+				pointX = (mc_[s + 1].first * radius_) + px_;
+				pointY = (mc_[s + 1].second * radius_) + py_;
+				
+				makeLine(lastX, lastY, pointX, pointY, &objectLines_);
+				
+				pointX = s < circlePoints + 1 ? pointX : mc_[1].first * radius_ + px_;
+				pointY = s < circlePoints + 1 ? pointY : mc_[1].second * radius_ + py_;
+				lastX = pointX;
+				lastY = pointY;
+			}
+			else
+			{
+				float phi = rotation_ * (M_PI / 180);
+				vx_ = cosf(phi) * radius_ + px_;
+				vy_ = -sinf(phi) * radius_ + py_;
+
+				makeLine(px_, py_, vx_, vy_, &objectLines_);
+			} 	
+		}
+	}
+
+
+	void moveDirection(float stepSize)
+	{
+		float phi = rotation_ * (M_PI / 180);
+		px_ = cosf(phi) * stepSize + px_;
+		py_ = -sinf(phi) * stepSize + py_;
+	}
+
+
+
+public:
+	float px_;
+	float py_;
+	float vx_;
+	float vy_;
+	float radius_;
+	float rotation_ = 0;
+
+private:
+	std::vector<fPair> mc_;
+	
+};
+
+class Pico : public olc::PixelGameEngine
+{
+private:
+	int levelWidth;
+	int levelHeight;
+	std::string levelTiles;
+	int tileWidth = 8;
+	int tileHeight = 8;
+
+	std::vector<fPair> modelCircle;
+	std::vector<Ball*> balls;
+	std::vector<Object*> objects;
+
+public:
+	Pico()
+	{
+		sAppName = "Pico";
+	}
+
+	bool OnUserCreate() override
+	{
+
+		/*
+		modelCircle.push_back({ 0.0f, 0.0f });
+		
+		for(int i = 0; i <= circlePoints; i++)
+		{
+			modelCircle.push_back({ cosf(i / (float)(circlePoints -1) * 2.00f * 3.14159f) , sinf(i / (float)(circlePoints - 1) * 2.0f * 3.14159f) });
+		}
+		*/
+
+		Ball* b1 = new Ball(100.0, 100.0, 0.0, 0.0, 50.0, 0.0f);
+		objects.push_back(b1);
+
+		
+
+		levelWidth = 32;
+		levelHeight = 32;
+		levelTiles += "################################";
+		levelTiles += "#..............................#";
+		levelTiles += "#..............................#";
+		levelTiles += "#..............................#";
+		levelTiles += "#..............................#";
+		levelTiles += "#..............................#";
+		levelTiles += "#..............................#";
+		levelTiles += "#..............................#";
+		levelTiles += "#..............................#";
+		levelTiles += "#..............................#";
+		levelTiles += "#..............................#";
+		levelTiles += "#..............................#";
+		levelTiles += "#..............................#";
+		levelTiles += "#..............................#";
+		levelTiles += "#..............................#";
+		levelTiles += "#..............................#";
+		levelTiles += "#..............................#";
+		levelTiles += "#..............................#";
+		levelTiles += "#..............................#";
+		levelTiles += "#..............................#";
+		levelTiles += "#..............................#";
+		levelTiles += "#..............................#";
+		levelTiles += "#..............................#";
+		levelTiles += "#..............................#";
+		levelTiles += "#..............................#";
+		levelTiles += "#..............................#";
+		levelTiles += "#..............................#";
+		levelTiles += "#..............................#";
+		levelTiles += "#..............................#";
+		levelTiles += "#..............................#";
+		levelTiles += "#..............................#";
+		levelTiles += "################################";
+
+		for(int x = 0; x < ScreenWidth(); x++)
+		{
+			for(int y = 0; y < ScreenHeight(); y++)
+			{
+				Draw(x, y, olc::BLUE);
+			}
+		}
+
+		/*
+		for(auto ball : balls)
+		{
+			for(int s = 0; s <= circlePoints; s++)
+			{
+				float pointX = (modelCircle[s].first * ball->radius_) + ball->px_;
+				float pointY = (modelCircle[s].second * ball->radius_) + ball->py_;
+
+				Draw(pointX, pointY, olc::WHITE);
+			}
+		}*/
+
+		SetPixelMode(olc::Pixel::NORMAL);
+
+		return true;
+	}
+
+	bool OnUserUpdate(float fElapsedTime) override
+	{
+		//User input
+
+		if(GetKey(olc::Key::UP).bHeld) static_cast<Ball*>(objects[0])->moveDirection(1.0f);
+		if(GetKey(olc::Key::DOWN).bHeld) static_cast<Ball*>(objects[0])->moveDirection(-1.0f);
+		if(GetKey(olc::Key::RIGHT).bHeld) static_cast<Ball*>(objects[0])->rotation_ += 1.0f;
+		if(GetKey(olc::Key::LEFT).bHeld) static_cast<Ball*>(objects[0])->rotation_ -= 1.0f;		
+		
+		//Collision detection
+
+		for(auto o : objects)
+		{
+			o->calculateShape();
+		}
+
+		for(int x = 0; x < ScreenWidth(); x++)
+		{
+			for(int y = 0; y < ScreenHeight(); y++)
+			{
+				Draw(x, y, olc::BLUE);
+			}
+		}
+
+		for(auto obj : objects)
+		{
+			for(auto lines : obj->objectLines_)
+			{
+				for(auto line : lines)
+				Draw(line.first, line.second, olc::BLACK);
+			}
+		}
+
+		return true;
+	}
+
+	
 
 };
 
