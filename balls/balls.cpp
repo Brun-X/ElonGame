45bb70f871002b3d8e93c4a452c8d13df2e36f64
Your Name <you@example.com>
2019-12-15 09:25:35 +0100

    add more drawing stuff

--- balls/balls.cpp
+++ balls/balls.cpp
@@ -16,6 +16,9 @@ IMPLEMNTED WITH THE OLCPIXELGAMEENGINE FROM:
 #include "olcPixelGameEngine.h"
 #include <cmath>
 #include <vector>
+#include <boost/shared_ptr.hpp>
+
+enum { BALL = 1, BOX=2, LINE=3 };
 
 
 typedef std::pair <float, float> fPair;
@@ -27,6 +30,7 @@ class Object
 {
 public:
 	std::vector<std::vector<iPair>> objectLines_;
+	int id_;
 	
 	virtual ~Object() {}
 
@@ -178,18 +182,71 @@ public:
 	std::vector<iPair> line_;
 };
 
+class Box : public Object
+{
+public:
+	float x1_;
+	float y1_;
+	float x2_;
+	float y2_;
+
+public:
+	Box() : Object() {};
+
+	Box(float x1, float y1, float x2, float y2) : x1_(x1), y1_(y1), x2_(x2), y2_(y2)
+	{
+		makeLine(x1_, y1_, x2_, y1_, &objectLines_);
+		makeLine(x2_, y1_, x2_, y2_, &objectLines_);
+		makeLine(x2_, y2_, x1_, y2_, &objectLines_);
+		makeLine(x1_, y2_, x1_, y1_, &objectLines_);
+	}
+
+	virtual void calculateShape() override {}
+};
+
 class Ball : public Object
 {
 public:
 	Ball() : Object() {}
 
-	Ball(int id, float px, float py, float vx, float vy, float radius, float rotation) : id_(id), px_(px), py_(py), vx_(vx), vy_(vy), radius_(radius), rotation_(rotation)
+	Ball(float px, float py, float vx, float vy, float radius, float rotation) : px_(px), py_(py), vx_(vx), vy_(vy), radius_(radius), rotation_(rotation)
 	{
 		mc_.push_back({ 0.0f, 0.0f });
 		
 		for(int i = 0; i <= circlePoints; i++)
 		{
 			mc_.push_back({ cosf(i / (float)(circlePoints -1) * 2.00f * 3.14159f) , sinf(i / (float)(circlePoints - 1) * 2.0f * 3.14159f) });
+		}
+
+		if(radius_ < 0)
+		{
+			radius_ = sqrt((fabs(px - vx) * fabs(px - vx)) + (fabs(py - vy) * fabs(py - vy)));
+
+			rotation_ = acosf((fabs(px - vx) / radius));
+		}
+
+		float pointX, pointY;
+		float lastX = mc_[1].first * radius_ + px_;
+		float lastY = mc_[1].second * radius_ + py_;
+
+		for(int s = 0; s < circlePoints + 1; s++)
+		{
+			if(s > 1) 
+			{
+				pointX = (mc_[s + 1].first * radius_) + px_;
+				pointY = (mc_[s + 1].second * radius_) + py_;
+				
+				makeLine(lastX, lastY, pointX, pointY, &objectLines_);
+				
+				pointX = s < circlePoints + 1 ? pointX : mc_[1].first * radius_ + px_;
+				pointY = s < circlePoints + 1 ? pointY : mc_[1].second * radius_ + py_;
+				lastX = pointX;
+				lastY = pointY;
+			}
+			else
+			{
+				makeLine(px_, py_, vx_, vy_, &objectLines_);
+			} 	
 		}
 	}
 
@@ -236,7 +293,6 @@ public:
 
 
 public:
-	int id_;
 	float px_;
 	float py_;
 	float vx_;
@@ -260,11 +316,12 @@ private:
 
 	std::vector<iPair> grid_;
 	int gridSize_ = 32;
-	std::vector<Ball*> balls;
+	unsigned int shapeId = 0;
 	std::vector<Object*> objects;
 	iPair newMousePos;
 	iPair lastMousePos;
 	bool drawing_ = false;
+	Object* drawingObject = nullptr;
 
 public:
 	Pico()
@@ -274,9 +331,6 @@ public:
 
 	bool OnUserCreate() override
 	{
-
-		//grid_.push_back({ 0, 0 });
-		
 		for(int x = 0; x < ScreenWidth() / gridSize_; x++)
 		{
 			for(int y = 0; y < ScreenHeight() / gridSize_; y++)
@@ -286,10 +340,13 @@ public:
 		}
 
 
+		objects.push_back(new Box(20.0f, 20.0f, 150.0f, 150.0f));
+
+
 		/*
 		for(int i = 0; i < 3; i++)
 		{
-			objects.push_back( new Ball(i, ((rand() + 10) % (ScreenWidth() - 10)), ((rand() + 10) % (ScreenHeight() - 10)), 0.0, 0.0, (float)((1 + (rand() % 5)) * 8), 0.0f));
+			objects.push_back( new Ball(((rand() + 10) % (ScreenWidth() - 10)), ((rand() + 10) % (ScreenHeight() - 10)), 0.0, 0.0, (float)((1 + (rand() % 5)) * 8), 0.0f));
 			
 		}
 	*/
@@ -339,18 +396,6 @@ public:
 			}
 		}
 
-		/*
-		for(auto ball : balls)
-		{
-			for(int s = 0; s <= circlePoints; s++)
-			{
-				float pointX = (modelCircle[s].first * ball->radius_) + ball->px_;
-				float pointY = (modelCircle[s].second * ball->radius_) + ball->py_;
-
-				Draw(pointX, pointY, olc::WHITE);
-			}
-		}*/
-
 		SetPixelMode(olc::Pixel::NORMAL);
 
 		return true;
@@ -360,36 +405,50 @@ public:
 	{
 		//User input
 
-
-
+		Ball tempBall;
+		Box tempBox;
+		Line tempLine;
+/*
 		if(GetKey(olc::Key::UP).bHeld) static_cast<Ball*>(objects[0])->moveDirection(1.0f);
 		if(GetKey(olc::Key::DOWN).bHeld) static_cast<Ball*>(objects[0])->moveDirection(-1.0f);
 		if(GetKey(olc::Key::RIGHT).bHeld) static_cast<Ball*>(objects[0])->rotation_ += 1.0f;
 		if(GetKey(olc::Key::LEFT).bHeld) static_cast<Ball*>(objects[0])->rotation_ -= 1.0f;		
+*/
 
 
-
-		
 
 		if(GetMouse(0).bPressed) 
 		{
-			//std::vector<std::vector<iPair>> movingLine;
 			lastMousePos = {GetMouseX(), GetMouseY()};
 			drawing_ = true;
-			//underConstruction_->push_back(lastMousePos);
-			//new Line(GetMouseX(), GetMouseY(), GetMouseX(), GetMouseY())
-			//underConstruction_.push_back();
+
+			if(GetKey(olc::Key::B).bHeld) shapeId = BOX;
+			else if(GetKey(olc::Key::C).bHeld) shapeId = BALL;
+			else if(GetKey(olc::Key::L).bHeld) shapeId = LINE;
+			else shapeId = 0;
 		}
 
 		if(GetMouse(0).bHeld) 
 		{
 			newMousePos = {GetMouseX(), GetMouseY()};
+			/*switch(shapeId)
+			{
+				case BALL :
+
+					break;
+			}*/
 		}
 
 		if(GetMouse(0).bReleased)
 		{
-			objects.push_back(new Line(lastMousePos.first, lastMousePos.second, newMousePos.first, newMousePos.second));
+			if(shapeId == BOX) objects.push_back(new Box(lastMousePos.first, lastMousePos.second, newMousePos.first, newMousePos.second));
+			
+			else if(shapeId == BALL) objects.push_back(new Ball((float)lastMousePos.first, (float) lastMousePos.second, (float) newMousePos.first, (float) newMousePos.second, -1.0f, 0.0f));
+
+			if(shapeId == LINE) objects.push_back(new Line(lastMousePos.first, lastMousePos.second, newMousePos.first, newMousePos.second));
+
 			drawing_ = false;
+			drawingObject = nullptr;
 		}
 
 
@@ -425,21 +484,60 @@ public:
 
 		if(drawing_)
 		{
-			std::vector<std::vector<iPair>> movingLine;
-			Object::makeLine(lastMousePos.first, lastMousePos.second, newMousePos.first, newMousePos.second, &movingLine);
-			for(auto lines : movingLine)
+			
+
+			switch(shapeId)
 			{
-				for(auto line: lines)
+				case BOX :
+					{
+					Box box(lastMousePos.first, lastMousePos.second, newMousePos.first, newMousePos.second);
+					tempBox = box;
+					//std::cout << "test" << std::endl;
+					drawingObject = &tempBox;
+					break;
+					}
+
+				case BALL :
+					{
+					Ball ball((float)lastMousePos.first, (float) lastMousePos.second, (float) newMousePos.first, (float) newMousePos.second, -1.0f, 0.0f);
+					tempBall = ball; 
+					drawingObject = &tempBall;
+
+/*					drawingObject = static_cast<Object>(ball);
+					std::cout << "shared drawingObject: " << drawingObject.use_count() << std::endl;
+					std::cout << "shared tempballptr: " << tempBall.use_count() << std::endl;
+*/
+					break;
+					}
+
+				case LINE :
+					{
+					Line line(lastMousePos.first, lastMousePos.second, newMousePos.first, newMousePos.second); 
+					tempLine = line;
+					drawingObject = &tempLine;
+					//Object::makeLine(lastMousePos.first, lastMousePos.second, newMousePos.first, newMousePos.second, &movingLine);
+					break;
+					}
+
+				default :
+					drawingObject = nullptr;
+					break;
+			}
+
+			//Box b(lastMousePos.first, lastMousePos.second, newMousePos.first, newMousePos.second);
+			if(drawingObject != nullptr)
+			{
+				for(auto lines : drawingObject->objectLines_)
 				{
-					Draw(line.first, line.second, olc::RED);
+					for(auto line: lines)
+					{
+						Draw(line.first, line.second, olc::RED);
+					}
 				}
 			}
 		}
 		return true;
 	}
-
-	
-
 };
 
 
